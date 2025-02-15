610f1d6ce9d4111f97c80901c0fa15cd2b72eb94
Andi <cska8006@gmail.com>
2025-02-15 13:31:33 +0100

    0215

---
+++ app/Http/Controllers/API/UserController.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Http\Controllers\API\ResponseController;
+use App\Models\User;
+use App\Http\Requests\RegisterRequest;
+use App\Http\Requests\LoginRequest;
+use Carbon\Carbon;
+use Illuminate\Support\Facades\Auth;
+
+
+class UserController extends ResponseController
+{
+    public function register(RegisterRequest $request){
+
+        $request->validated();
+
+        $user = User::create([
+            "name"=>$request ["name"],
+            "email"=>$request ["email"],
+            "password"=>bcrypt ($request ["password"]),
+        ]);
+
+        return $user;
+    }
+
+    public function login(LoginRequest $request){
+
+        $request->validated();
+
+        if (Auth::attempt(["name"=>$request["name"], "password"=>$request["password"]])){
+
+        $authUser = Auth::user();
+        $token = $authUser->createToken($authUser->name."token")->plainTextToken;
+        $data = [
+            "name"=> $authUser->name,
+            "token"=> $token
+        ];
+
+        return $this->sendResponse($data, "Sikeres bejelentkezés!");  
+        }
+    }
+
+    
+    
+    public function logout(Request $request) {
+         
+        $user = auth( "sanctum" )->user();
+        $user()->currentAccessToken()->delete();
+
+        return $this->sendResponse( $user->name, "Sikeres kijelentkezés" );
+    }
+    
+
+}
+
+    
--- routes/api.php
+++ routes/api.php
@@ -3,10 +3,17 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\API\AnimalController;
+use App\Http\Controllers\API\UserController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::post("/register", [UserController::class, "register"]);
+Route::post("/login", [UserController::class, "login"]);
+Route::post("/logout", [UserController::class, "logout"])->middleware("auth:sanctum");
+  
+
 
 Route::get( "/animals", [ AnimalController::class, "getAnimals" ]);
 
